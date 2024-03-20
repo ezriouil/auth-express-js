49fb0b763803b34d2255c681f4457a5232d0d98e
ezriouil <ezriouil2003@gmail.com>
2024-03-20 15:23:52 +0000

    new update

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -45,7 +45,7 @@ const register = async (req, res) => {
     const { password, isAdmin, verified, otpCode, __v, ...other } = result._doc;
 
     const token = jwt.sign({ email : user.email, id : user._id }, process.env.JWT_SECRET_KEY)
-    const link = `http://localhost:8000/api/auth/verify-user-account/${token}`;
+    const link = `https://sparkling-rose-hosiery.cyclic.app/api/auth/verify-user-account/${token}`;
 
     const transporter = nodeMailer.createTransport({
             service : "gmail",
@@ -152,7 +152,7 @@ const reSendEmailVerificationLink = async(req, res) => {
   }
 
   const token = jwt.sign({ email : user.email, id : user._id }, process.env.JWT_SECRET_KEY)
-  const link = `http://localhost:8000/api/auth/verify-user-account/${token}`;
+  const link = `https://sparkling-rose-hosiery.cyclic.app/api/auth/verify-user-account/${token}`;
 
   const transporter = nodeMailer.createTransport({
             service : "gmail",
